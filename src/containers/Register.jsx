38399250791031d2d52287c802a6a8814f867f7f
santechx <santiagogf11@gmail.com>
2020-07-02 00:24:23 -0500

    routes created and redux setup

---
+++ src/containers/Register.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../assets/styles/components/Register.scss';
+
+const Register = () => (
+    <section className="register">
+        <section className="register__container">
+            <h2>Regístrate</h2>
+            <form className="register__container--form">
+                <input className="register__input" type="text" placeholder="Nombre" />
+                <input className="register__input" type="text" placeholder="Correo" />
+                <input className="register__input" type="password" placeholder="Contraseña" />
+                <button className="register__button">Registrarme</button>
+            </form>
+            <Link to="/login">
+                Iniciar sesión
+            </Link>
+        </section>
+    </section>
+);
+
+export default Register;
